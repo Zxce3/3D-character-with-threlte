d28e5bc4f6ac3b0858fb55c4986acc0bce34167e
Memet Zx <zxce3@gnuweeb.org>
2025-02-10 00:53:17 +0700

    Add Scene component with camera and world elements

---
+++ src/lib/components/Scene.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { T } from '@threlte/core'
+    import { interactivity } from '@threlte/extras'
+    import Character from '$lib/components/Character.svelte'
+    import type { CharacterActions } from '$lib/types'
+    import World from './World.svelte'
+    import Ground from './Ground.svelte'
+  
+    export let action: CharacterActions;
+    interactivity()
+</script>
+  
+<T.PerspectiveCamera
+    makeDefault
+    position={[-0.85, 1.75, 2.46]}
+    oncreate={(ref) => {
+        ref.lookAt(0, 1, 0)
+    }}
+/>
+
+<World />
+<Character actionKey={action as CharacterActions} />
+<Ground />
